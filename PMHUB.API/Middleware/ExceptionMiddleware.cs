78419f0e2234d94395c565158093692319fe9e4a
elmehdidouiri <elmehdidouiri99@gmail.com>
2026-05-05 09:38:12 +0100

    Email sender done

--- PMHUB.API/Middleware/ExceptionMiddleware.cs
+++ PMHUB.API/Middleware/ExceptionMiddleware.cs
@@ -33,7 +33,7 @@ namespace PMHUB.API.Middleware
             }
         }
 
-        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
         {
             context.Response.ContentType = "application/json";
 
